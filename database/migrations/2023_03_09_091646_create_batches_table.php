2642097db3a5358e864299f7e266a72ce59c7a07
aomkhamphaung <akhamphaung@gmail.com>
2023-03-10 15:25:38 +0630

    added files

---
+++ database/migrations/2023_03_09_091646_create_batches_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('batches', function (Blueprint $table) {
+            $table->id();
+            $table->string('batch_name');
+            $table->unsignedBigInteger('course_id');
+            $table->foreign('course_id')
+                ->references('id')->on('courses')
+                ->onDelete('cascade');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('batches');
+    }
+}
