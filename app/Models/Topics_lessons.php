a97fc2efe268530f8ba3fd8d7841396cd8c0fbeb
Ye Htun <phoechwe439@gmail.com>
2023-03-10 15:30:33 +0630

    firstCommit

---
+++ app/Models/Topics_lessons.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Topics_lessons extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2023_03_09_090904_create_trainer_specialities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainer_specialities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('trainer_id');
+            $table->foreign('trainer_id')
+                    ->references('id')->on('trainers')
+                    ->onDelete('cascade');
+            $table->unsignedBigInteger('speciality_id');
+            $table->foreign('speciality_id')
+                    ->references('id')->on('specialities')
+                    ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainer_specialities');
+    }
+};
