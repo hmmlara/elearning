91f19a04aef689c2382e39abfd357cc22612fb87
Pyae Phyo Zan <pyae418283@gmail.com>
2023-03-10 19:08:33 +0630

    add and change route files

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::group(['namespace' => 'App\Http\Controllers'],function(){
+    Route::group(['prefix' => 'admin'],function(){
+        Route::view('home','admin.index');
+    });
 });
