368b5e2b52984e2cca36a74bd4b782bbe905aeb2
chotay13599 <chotay13599@gmail.com>
2023-03-10 15:37:22 +0630

    first commit

---
+++ database/migrations/2023_03_10_022833_create_topics_lessons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics_lessons', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('topic_id');
+            $table->foreign('topic_id')
+                ->references('id')->on('topics')
+                ->onDelete('cascade');
+            $table->string('lesson_name');
+            $table->string('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topics_lessons');
+    }
+};
---
+++ database/migrations/2023_03_10_023154_create_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('register_id');
+            $table->foreign('register_id')
+                ->references('id')->on('registers')
+                ->onDelete('cascade');
+            $table->integer('payment_amount');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
