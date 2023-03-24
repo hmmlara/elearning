5b2590e205b38522a8bc9119d81630bed7bb90ee
Pyae Phyo Zan <pyae418283@gmail.com>
2023-03-25 00:04:51 +0630

    add eloquant with course

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'course_id',
+        'day',
+        'start_time',
+        'end_time'
+    ];
+
+    public function course(){
+        return $this->belongsTo(Course::class);
+    }
 }
