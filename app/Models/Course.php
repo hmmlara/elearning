2d696f39d73387276e0fd09fc299e5a3fa208f26
Pyae Phyo Zan <pyae418283@gmail.com>
2023-03-25 00:04:17 +0630

    add eloquant with category and schedule

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function schdules(){
+        return $this->hasMany(Schedule::class);
+    }
 }
