b2578e3a5f1cf9710d96e9f32da8aa974125bf9a
Ye Htun <phoechwe439@gmail.com>
2023-03-25 10:34:43 +0630

    add routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TrainerController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,10 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-})->name('index');
 
+
+Route::resource('/',HomeController::class);
 Route::get('/about',function (){
     return view('about');
 })->name('about');
