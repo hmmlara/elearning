0121f056fedacd5aa4b45f19e1764c80b25692e3
Ye Htun <phoechwe439@gmail.com>
2023-03-25 10:34:14 +0630

    change code

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,28 +12,6 @@ class Category extends Model
     public function courses()
     {
         return $this->hasMany(Course::class);
-    //   $thid: <a href=" fdak "></a>;
-    //   dafslkj
-//       $thid dflaskfapdfa
-//   $thduas;
-//   dklsh fclose
-//   fdaslk 
-//   kdfas 
-//   dkflasdfklsa
-//   dfksa
-//   fdslakf
-//   fdsaklf
-//   dkfl
-//   kfdl
-//   diskfreespaceflkd;''
-//   dlf;
-//   DkimSigner;
-//   dba_key_splitd 
-//   dkj
-//   ftp_mkdir
-//   dklf
-
-
   
     }
 }
--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
-  
+    protected $fillable=['title','description','duration','ojt_duration','hours','total_topics','fee','discount','learning_outcome','category_id'];
     public function category()
     {
         return $this->belongsTo(Category::class);
